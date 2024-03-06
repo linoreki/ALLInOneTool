0b67b51a27dd8b32fe1e4e47efa660d5640e1ba4
linoreki <linorekigames@gmail.com>
2024-03-06 16:41:25 +0100

    changed the way that metasploit install

--- Alltools instalation.py
+++ Alltools instalation.py
@@ -18,9 +18,6 @@ def instalar_msfconsole():
     subprocess.run(["sudo", "apt-get", "install", "build-essential", "zlib1g", "zlib1g-dev", "libxml2", "libxml2-dev", "libxslt-dev", "locate", "libreadline6-dev", "libcurl4-openssl-dev", "git-core", "autoconf", "curl", "postgresql", "postgresql-contrib", "libpq-dev", "libapr1", "libaprutil1", "libsvn1", "libpcap-dev","ruby" "-y"])
     subprocess.run(["git", "clone", "https://github.com/rapid7/metasploit-framework.git"])
     os.chdir(f"{os.getcwd()}/metasploit-framework/")
-
-    
-    
     subprocess.run(["sudo", "bash", "-c", "'for MSF in $(ls msf*) do ln -s /usr/local/src/metasploit-framework/$MSF /usr/local/bin/$MSF'"])
     subprocess.run(["sudo","gem" ,"install", "bundler", "-v" ,"2.4.22"])
     subprocess.run(["sudo","apt-get" ,"install" ,"ruby-full" ,"build-essential"])
@@ -30,19 +27,23 @@ def instalar_msfconsole():
     subprocess.run(["sudo", "service", "postgresql", "start"])
     subprocess.run([ "msfdb", "init"])
 
-def instalar_aplicaciones():
 
-    sistema = platform.system()
-    print(sistema)
-    
-    
+def instalar_simple():
+    subprocess.run(["sudo","apt","update"])
     subprocess.run(["sudo", "gem", "install", "wpscan"])
     subprocess.run(["sudo", "snap", "install", "searchsploit"])
     subprocess.run(["sudo", "apt", "install", "nmap", "-y"])
+    subprocess.run(["sudo","snap","install","metasploit-framework"])
 
-    #subprocess.run(["wget http://downloads.metasploit.com/data/releases/metasploit-latest-linux-x64-installer.run"])
-    # subprocess.run(["sudo chmod +x ./metasploit-latest-linux-x64-installer.run"])
-    # subprocess.run(["./metasploit-latest-linux-x64-installer.run"])
+
+def instalar_linux():
+    instalar_simple()
+
+def instalar_aplicaciones():
+
+    sistema = platform.system()
+    if sistema == Linux:
+        instalar_linux()
 
 def ejecutar_aplicacion_1():
     
@@ -68,41 +69,46 @@ def menu():
     
     print("Bienvenido al Menú Hacker:")
     print("1. Instalar TODAS las aplicación 1")
-    print("2. Ejecutar aplicación 1")
-    print("3. Ejecutar aplicación 2")
-    print("4. Ejecutar aplicación 3")
-    print("5. Ejecutar aplicación 4")
-    print("6. Salir")
+    print("2. instalar metasploit (era compleja)")
+    print("3. Ejecutar aplicación 1")
+    print("4. Ejecutar aplicación 2")
+    print("5. Ejecutar aplicación 3")
+    print("6. Ejecutar aplicación 4")
+    print("7. Salir")
 
-    opcion = int(input("Por favor, elige una opción (1-6): "))
+    opcion = int(input("Por favor, elige una opción (1-7): "))
 
     
     if opcion == 1:
         instalar_aplicaciones()
         return
-
-    elif opcion == 2:
-        ejecutar_aplicacion_1()
+    
+    if opcion == 2:
+        instalar_msfconsole()
         return
     
     elif opcion == 3:
-        ejecutar_aplicacion_2()
+        ejecutar_aplicacion_1()
         return
     
     elif opcion == 4:
-        ejecutar_aplicacion_3()
+        ejecutar_aplicacion_2()
         return
     
     elif opcion == 5:
-        ejecutar_aplicacion_4()
+        ejecutar_aplicacion_3()
         return
     
     elif opcion == 6:
+        ejecutar_aplicacion_4()
+        return
+    
+    elif opcion == 7:
         print("¡Hasta luego!")
         time.sleep(2)
         exit()
     
-    elif opcion >= 6:
+    elif opcion >= 7:
         print("opcion incorrecta por favor inserte un valor permitido")
         menu()
 
