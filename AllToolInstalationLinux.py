da8673ec2f8d2db5bc1cc9ab41db72797643f9f8
linoreki <linorekigames@gmail.com>
2024-03-15 08:12:15 +0100

    bug fixes

--- AllToolInstalationLinux.py
+++ AllToolInstalationLinux.py
@@ -48,7 +48,10 @@ def instalar_simple():
     execute("sudo git clone https://github.com/Muxutruk2/ahaikatu")
     print("instalando Msfconsole")
     execute("sudo snap install metasploit-framework")
-
+    execute("sudo apt install ca-certificates gnupg")
+    execute("sudo gpg --homedir /tmp --no-default-keyring --keyring /usr/share/keyrings/mono-official-archive-keyring.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF")
+    execute("echo \"deb [signed-by=/usr/share/keyrings/mono-official-archive-keyring.gpg] https://download.mono-project.com/repo/ubuntu stable-focal main\" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list")
+    execute("sudo apt update")
     msfsi = input("Te ha dado error msfconsole?(Y/N)").lower()
     if msfsi == "y":
        instalar_msfconsole()
